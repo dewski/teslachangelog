f60b486320383cf3c5b4fd7758ce5c66e9be0f21
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2018-11-12 21:13:22 -0500

    Don't generate everything

---
+++ config/initializers/generators.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.generators do |generate|
+  generate.helper false
+  generate.assets false
+  generate.view_specs false
+  generate.skip_routes true
+end
+
